e6995165c8bf944e057d9f4f2d3f1dac94f45f77
Yesmimo <Yesmimo6@gmail.com>
2023-01-23 12:02:41 +0100

    Nuovo Sito

---
+++ Js/index.js
@@ -0,0 +1,26 @@
+const img=[];
+let index=0;
+
+function caroselloStart(){
+    img.push("Immagini/ludoBanner.png");
+    img.push("Immagini/slotBanner.png");
+    img.push("Immagini/thelemaBanner.png");
+    setInterval(function(){
+        document.getElementById("slideCarosello").style.backgroundImage="url("+img[index]+")";
+        index++;
+        if(index>=img.length){
+            index=0;
+        }
+    },8000);
+}
+
+function muovi(a){
+    index=index+a;
+    if(index>=img.length){
+        index=0;
+    }
+    if(index<0){
+        index=img.length-1;
+    }
+    document.getElementById("slideCarosello").style.backgroundImage="url("+img[index]+")";
+}
