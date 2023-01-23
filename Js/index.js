8a7a157d3eb84a25a336b7610e8a6a7395c7e3ba
Yesmimo <Yesmimo6@gmail.com>
2023-01-23 12:07:34 +0100

    Prova per caricare tutte le immagini

--- Js/index.js
+++ Js/index.js
@@ -5,6 +5,10 @@ function caroselloStart(){
     img.push("Immagini/ludoBanner.png");
     img.push("Immagini/slotBanner.png");
     img.push("Immagini/thelemaBanner.png");
+    document.getElementById("slideCarosello").style.backgroundImage="url('Immagini/slotBanner.png')";
+    document.getElementById("slideCarosello").style.backgroundImage="url('Immagini/thelemaBanner.png')";
+    document.getElementById("slideCarosello").style.backgroundImage="url('Immagini/thelemaBanner.png')";
+
     setInterval(function(){
         document.getElementById("slideCarosello").style.backgroundImage="url("+img[index]+")";
         index++;
