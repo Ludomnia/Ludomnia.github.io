ec7fca6889029f86320469b7a3228e74ed780239
Yesmimo <Yesmimo6@gmail.com>
2023-01-29 17:56:26 +0100

    aggiunta pagina e bottoni
    aggiunta pagina thelema (solo sfondo) e ora i link alla pagina thelema vanno (e alla home)

--- Js/index.js
+++ Js/index.js
@@ -1,16 +1,26 @@
 const img=[];
+const pagina=[];
 let index=0;
 
 function caroselloStart(){
+    //inserisci nell'array le immagini
     img.push("Immagini/ludoBanner.png");
     img.push("Immagini/slotBanner.png");
     img.push("Immagini/thelemaBanner.png");
+
+    //carica le immagini
     document.getElementById("slideCarosello").style.backgroundImage="url('Immagini/slotBanner.png')";
     document.getElementById("slideCarosello").style.backgroundImage="url('Immagini/thelemaBanner.png')";
     document.getElementById("slideCarosello").style.backgroundImage="url('Immagini/ludoBanner.png')";
 
+    //inserisci i link corrispondenti alle pagine delle immagini
+    pagina.push("index.html");
+    pagina.push("index.html");
+    pagina.push("pagine/thelema.html");
+
     setInterval(function(){
         document.getElementById("slideCarosello").style.backgroundImage="url("+img[index]+")";
+        document.getElementById("link-carosello").href=pagina[index];
         index++;
         if(index>=img.length){
             index=0;
@@ -27,4 +37,5 @@ function muovi(a){
         index=img.length-1;
     }
     document.getElementById("slideCarosello").style.backgroundImage="url("+img[index]+")";
+    document.getElementById("link-carosello").href=pagina[index];
 }
